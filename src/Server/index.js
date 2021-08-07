414bf38a767ca647c85eb92ac4ad65f5449ea2ef
arlw87 <arlw87@googlemail.com>
2021-08-08 00:20:07 +0100

    chore: set up basic express web server

---
+++ src/Server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded(({extended: false})));
+app.use(bodyParser.json());
+app.use(express.static('./src/public'));
+
+// app.get('/', function(req, res){
+//     res.send('Hello World!');
+// });
+
+//start the server
+const port = 3000;
+const listening = () => {
+    console.log("Server Running");
+}
+
+const server = app.listen(port, listening);
